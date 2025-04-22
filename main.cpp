ce42ba60a15887f5253132705fa397e89688b09a
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2025-04-22 21:36:34 +0200

    Replaced vector with raw 2D array, Added a destructor

--- main.cpp
+++ main.cpp
@@ -1,59 +1,95 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-
-using namespace std;
 
 class Graph {
 private:
     int V;
-    vector<vector<int>> adj;
+    int** adj;
+    int* in_degree;
+
+    void push(int* q, int& rear, int val) {
+        q[rear++] = val;
+    }
+
+    int pop(int* q, int& front) {
+        return q[front++];
+    }
 
 public:
-    Graph(int V) : V(V), adj(V) {}
+    Graph(int V) {
+        this->V = V;
+        adj = new int*[V];
+        for (int i = 0; i < V; ++i) {
+            adj[i] = new int[V];
+            for (int j = 0; j < V; ++j) {
+                adj[i][j] = 0;
+            }
+        }
+        in_degree = new int[V];
+        for (int i = 0; i < V; ++i) {
+            in_degree[i] = 0;
+        }
+    }
 
     void addEdge(int u, int v) {
-        adj[u].push_back(v);
+        adj[u][v] = 1;
     }
 
     bool topologicalSort() {
-        vector<int> in_degree(V, 0);
         for (int u = 0; u < V; ++u) {
-            for (int v : adj[u]) {
-                in_degree[v]++;
-            }
-        }
-
-        queue<int> q;
-        for (int i = 0; i < V; ++i) {
-            if (in_degree[i] == 0) {
-                q.push(i);
-            }
-        }
-
-        vector<int> topo_order;
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-            topo_order.push_back(u);
-
-            for (int v : adj[u]) {
-                if (--in_degree[v] == 0) {
-                    q.push(v);
+            for (int v = 0; v < V; ++v) {
+                if (adj[u][v] == 1) {
+                    in_degree[v]++;
                 }
             }
         }
 
-        if (topo_order.size() != V) {
-            cout << "Cycle detected!" << endl;
+        int* queue = new int[V];
+        int front = 0, rear = 0;
+
+        for (int i = 0; i < V; ++i) {
+            if (in_degree[i] == 0) {
+                push(queue, rear, i);
+            }
+        }
+
+        int* topo_order = new int[V];
+        int idx = 0;
+
+        while (front < rear) {
+            int u = pop(queue, front);
+            topo_order[idx++] = u;
+
+            for (int v = 0; v < V; ++v) {
+                if (adj[u][v] == 1) {
+                    in_degree[v]--;
+                    if (in_degree[v] == 0) {
+                        push(queue, rear, v);
+                    }
+                }
+            }
+        }
+
+        if (idx != V) {
+            std::cout << "Cycle detected!" << std::endl;
             return false;
         }
 
-        for (int node : topo_order) {
-            cout << node << " ";
+        for (int i = 0; i < V; ++i) {
+            std::cout << topo_order[i] << " ";
         }
-        cout << endl;
+        std::cout << std::endl;
+
+        delete[] queue;
+        delete[] topo_order;
         return true;
+    }
+
+    ~Graph() {
+        for (int i = 0; i < V; ++i) {
+            delete[] adj[i];
+        }
+        delete[] adj;
+        delete[] in_degree;
     }
 };
 
@@ -67,7 +103,7 @@ int main() {
     g.addEdge(3, 1);
 
     if (!g.topologicalSort()) {
-        cout << "Graph has a cycle" << endl;
+        std::cout << "Graph has a cycle" << std::endl;
     }
 
     return 0;
